d5f46900b2e2893c71f0c063506864a265af27e5
rishikgoud <goudrishik2006@gmail.com>
2024-04-15 23:48:10 +0530

    added

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const createbutton = document.querySelector(".createnotes");
 const notescontainer = document.querySelector(".notescontainer");
 let notes = document.querySelectorAll(".notes");
 
+function showstorage(){
+    notescontainer.innerHTML = localStorage.getItem("notes");
+}
+showstorage();
+
+function updatestorage(){
+    localStorage.setItem("notes",notescontainer.innerHTML); 
+}
+
 createbutton.addEventListener("click", ()=>{
     let notespage = document.createElement("p");
     let noteimage = document.createElement("img");
@@ -11,4 +20,24 @@ createbutton.addEventListener("click", ()=>{
     notescontainer.appendChild(notespage).appendChild(noteimage);
 })  
 
-// alert("hi");
+notescontainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updatestorage();
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".notes")
+        notes.forEach(nt =>{
+            nt.onkeyup = function(){
+                updatestorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown",event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
